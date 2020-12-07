0218361be99f821d84ed457bb690dcd670670c55
Ekaterina Dranitsyna <ekaterina.dranitsyna@gmail.com>
2020-12-07 16:55:49 +0300

    Added exp smoothing forecast

--- trends_app/trends/forecast.py
+++ trends_app/trends/forecast.py
@@ -1,52 +1,85 @@
 """Модуль выполняет прогноз цен на основе ретроспективной динамики.
-Учитывается горизонт прогноза, выбранный пользователем.
-Из нескольких прогнозов на основе предшествующих временных рядов
-разной длины выбирается вариант, имеющий наиболее высокий показатель R2.
+Учитывается горизонт прогноза и метод, выбранный пользователем.
 """
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+
 import pandas as pd
 import numpy as np
 
 
+def get_forecast(data: pd.DataFrame, n_months: int, method: str) -> pd.DataFrame:
+    """Функция выполняет прогноз цен с применением выбранного метода."""
+
+    if method == 'linregression':
+        forecast = linear_trend(data, n_months)
+    elif method == 'expsmoothing':
+        forecast = exponential_smoothing(data, n_months)
+
+    return forecast
+
+
 def linear_trend(data: pd.DataFrame, n_months: int) -> pd.DataFrame:
     """Функция выполняет прогноз цен с использованием модели
-    линейной регрессии. Возвращает прогноз с оптимальным R2."""
+    линейной регрессии. Возвращает прогноз с оптимальным показателем R2."""
 
     best_r2 = -1
-    best_model = None
     best_period = None
 
     # Перебираем варианты временных рядов от 1/2 прогнозного периода до 4 прогнозных периодов:
-    for prev_period in (n_months // 2 + 1, n_months, n_months * 2, n_months * 3, n_months * 4):
-        X = np.array([i for i in range(prev_period * 30)]).reshape(-1, 1)
-        y = data['price'].tail(prev_period * 30)
+    n_days = n_months * 30
+    for prev_period in (n_days // 2, n_days, n_days * 2, n_days * 3, n_days * 4):
+        X = np.array([i for i in range(prev_period)]).reshape(-1, 1)
+        y = data['price'].tail(prev_period)
 
         model = LinearRegression()
         kf = KFold(5, shuffle=True)
         r2 = cross_val_score(model, X, y, cv=kf, scoring='r2').mean()
 
         if r2 > best_r2:
-            best_r2 = r2
-            best_model = model
-            best_period = prev_period
+            best_r2, best_period = r2, prev_period
 
-    # Прогноз на основе оптимальной модели:
-    X = np.array([i for i in range(best_period * 30)]).reshape(-1, 1)
-    y = data['price'].tail(best_period * 30)
-    best_model.fit(X, y)
+    # Прогноз на основе оптимального временного ряда для обучения модели:
+    X = np.array([i for i in range(best_period)]).reshape(-1, 1)
+    y = data['price'].tail(best_period)
+    model = LinearRegression().fit(X, y)
 
-    trend_X = np.array([i for i in range(best_period * 30 + n_months * 30)]).reshape(-1, 1)
-    trend_y = best_model.predict(trend_X)
+    trend_X = np.array([i for i in range(best_period + n_days)]).reshape(-1, 1)
+    trend_y = model.predict(trend_X)
 
     # Преобразуем полученный результат в датафрейм:
     # линия тренда включает предшествующий и прогнозный период с датами и ценами.
-    start_day = data['date'].tail(best_period * 30).min()
-    periods = (best_period + n_months) * 30
-    trend_dates = pd.date_range(start=start_day, periods=periods, freq='D')
-    forecast_df = pd.DataFrame({'date': trend_dates, 'price': trend_y})
+    forecast_df = pd.DataFrame({'date': data['date'].tail(best_period)})
+    next_day = forecast_df['date'].max() + pd.to_timedelta('1 days')
+    periods = n_days
+    future_dates = pd.date_range(start=next_day, periods=periods, freq='D')
+    forecast_df = forecast_df.append(pd.DataFrame({'date': future_dates}))
+    forecast_df['price'] = trend_y
+
+    return forecast_df
+
+
+def exponential_smoothing(data: pd.DataFrame, n_months: int):
+    """Функция выполняет прогноз цен с использованием
+    модели экспоненциального сглаживания."""
+
+    # Используем для обучения модели период, равный двум прогнозным периодам:
+    prev_days = n_months * 30 * 2
+    model = ExponentialSmoothing(data['price'].tail(prev_days), trend='add').fit()
+    forecast = model.forecast(n_months * 30)
+
+    # Преобразуем полученный прогноз в датафрейм и включаем
+    # model.fittedvalues и будущий период с датами и ценами.
+    forecast_df = pd.DataFrame({'date': data['date'].tail(prev_days), 'price': model.fittedvalues})    
+
+    next_day = forecast_df['date'].max() + pd.to_timedelta('1 days')
+    periods = n_months * 30
+    future_dates = pd.date_range(start=next_day, periods=periods, freq='D')
+
+    forecast_df = forecast_df.append(pd.DataFrame({'date': future_dates, 'price': forecast}))
 
     return forecast_df
--- trends_app/trends/views.py
+++ trends_app/trends/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from trends.models import Price
 from django.contrib.auth.models import User
 
-from trends.forecast import linear_trend
+from trends.forecast import get_forecast
 
 from plotly.offline import plot
 from plotly.graph_objs import Scatter
@@ -27,6 +27,7 @@ def trend(request):
         try:
             item = request.GET['items']
             n_months = int(request.GET['months'])
+            method = request.GET['methods']
         except Exception:
             # Если пользователь вводит ссылку на /trend/ не выбрав категорию,
             # перенаправляем его на главную страницу сайта:
@@ -38,20 +39,82 @@ def trend(request):
     data.columns = ['date', 'price']
 
     # Делаем прогноз:
-    forecast = linear_trend(data, n_months)
+    forecast = get_forecast(data, n_months, method)
 
-    # График для отображения на странице:
-    fig_actual = Scatter(x=data['date'], y=data['price'],
-                  mode='lines', name='Динамика',
-                  opacity=0.8, marker_color='green')
-    fig_trend = Scatter(x=forecast['date'], y=forecast['price'],
-                  mode='lines', name='Прогноз',
-                  opacity=0.8, marker_color='red')
-    plot_div = plot([fig_actual, fig_trend], output_type='div')
- 
+    # График с фактическими значениями и прогнозом:
+    plot_div = plot_data(data, forecast)
+
+    # Таблица с ежемесячным прогнозом:
+    table_div, pred_price, pc_change = create_table(forecast, n_months)
+
     context = {
         'item': item_names[item],
-        'plot_div': plot_div
+        'plot_div': plot_div,
+        'table_div': table_div,
+        'pred_price': pred_price,
+        'pc_change': pc_change
     }
 
     return render(request, 'trends/trend.html', context=context)
+
+
+def plot_data(original_data: pd.DataFrame, forecast_data: pd.DataFrame):
+    """Функция формирует интерактивный график с динамикой цен и прогнозом
+    для отображения на html-странице."""
+
+    fig_actual = Scatter(x=original_data['date'], y=original_data['price'],
+                  mode='lines', name='Динамика',
+                  opacity=0.8, marker_color='green')
+
+    # Если получен корректный прогноз, отображаем его на графике с предшествующей динамикой.
+    if forecast_data['price'].isna().sum() == 0:
+        fig_trend = Scatter(x=forecast_data['date'], y=forecast_data['price'],
+                            mode='lines', name='Прогноз',
+                            opacity=0.8, marker_color='red')
+
+        plot_div = plot([fig_actual, fig_trend], output_type='div')
+    
+    # Если в прогнозе есть значения np.nan, показываем только динамику фактических данных.
+    else:
+        plot_div = plot([fig_actual], output_type='div')
+
+    return plot_div
+
+
+def create_table(forecast_data: pd.DataFrame, n_months: int):
+    """Функция формирует и возвращает элемент <table> с ежемесячным прогнозом цен
+    для отображения на html-странице, стоимость на конец прогнозного периода
+    и изменение по сравнению с последними фактическими данными в процентах."""
+
+    # Проверяем, что получен корректный прогноз
+    # (на некоторых временных отрезках экпоненциальное сглаживание
+    # приводит к ошибке: Optimization failed to converge и значениям np.nan).
+    if forecast_data['price'].isna().sum() == 0:
+
+        # Индексы строк в прогнозном датафрейме с шагом в 30 дней:
+        n_periods = len(forecast_data) // 30
+        monthly_indexes = [i * 30 - 1 for i in range(n_periods - n_months, n_periods + 1)]
+    
+        # Выбираем строки прогноза с шагом в 30 дней:
+        monthly_forecast = forecast_data.iloc[monthly_indexes, :].copy()
+        monthly_forecast['date'] = monthly_forecast['date'].apply(lambda x: x.date)
+        
+        # Цена на конец прогнозного периода и изменение в процентах к последним фактическим данным:
+        last_actual_price = monthly_forecast.iloc[0, :]['price']
+        last_forecasted_price = round(monthly_forecast.iloc[len(monthly_forecast) - 1, :]['price'], 2)
+        pc_change = str(round((last_forecasted_price / last_actual_price - 1) * 100, 2)) + '%'
+
+        # Создаем элемент html:
+        monthly_forecast.columns = ['Дата', 'Цена']
+        table = monthly_forecast.iloc[1:, :].to_html(index=False, 
+                                                    float_format='{:10.2f}'.format, 
+                                                    classes=['table', 'table-striped'], 
+                                                    justify='center')
+    
+    # Если из модели экспоненциального сглаживания получены значения np.nan,
+    # вместо прогнозных значений отображаем для пользователя сообщение об ошибке.
+    else:
+        table = ''
+        last_forecasted_price, pc_change = 'Ошибка', 'не удалось выполнить прогноз для выбранного периода'
+
+    return table, last_forecasted_price, pc_change
